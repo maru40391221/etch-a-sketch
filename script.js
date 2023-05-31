65048ac6d467c788596f6042617f53f987300334
maru40391221 <polorll.1221@gmail.com>
2023-05-31 20:08:41 +0900

    Created a column but do not know how to make rows

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
 const container = document.querySelector(".container");
-const smallButton = document.querySelector("#smallButton");
-const mediumButton = document.querySelector("#mediumButton");
-const largeButton = document.querySelector("#largeButton");
+let column;
 
-let numberOfBoxes = 16;
-let numberOfLines = 16;
+// create columns
+const numberOfColumn = 16;
+Array.from({length: numberOfColumn}, () => {
+    let box = document.createElement("div")
+    column.appendChild(box);
+    container.appendChild(column);
+    column.classList.add("column");
+});
 
-// container.style.display = "grid";
-// container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 0fr)`; 
+// create rows
 
-// mediumButton.onclick = ;
+const numberOfRow = 16;
+Array.from({length: numberOfrow}, () => {
+
+    container.appendChild(column);
+    column.classList.add("column");
+});
 
 
-function createMediumGrid (){
-//creating a line of boxes horizontally
-function createLines (){
-    for (let i = 0; i < numberOfLines; i++){
-        const line = document.createElement("div");
-        container.appendChild(line);
-        line.classList.add("line");
-        line.classList.add("colored");
-        console.log(i);
-    }
-}
 
-function createBoxes (){
-for (let i = 0; i < numberOfBoxes; i++){
-    createLines();
-}
-}
-createBoxes();
 
-}
 
-createMediumGrid();
+
+//  the codes below are the first attempt with grid-layout. I failed. Now I will try to make 16x16 grids with flexbox-layout. So they can shrink and whatnot. 
+// const container = document.querySelector(".container");
+// const smallButton = document.querySelector("#smallButton");
+// const mediumButton = document.querySelector("#mediumButton");
+// const largeButton = document.querySelector("#largeButton");
+
+// let numberOfBoxes = 16;
+// let numberOfLines = 16;
+
+// // container.style.display = "grid";
+// // container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 0fr)`; 
+// // mediumButton.onclick = ;
+
+
+// function createMediumGrid (){
+// //creating a line of boxes horizontally
+// function createLines (){
+//     for (let i = 0; i < numberOfLines; i++){
+//         const line = document.createElement("div");
+//         container.appendChild(line);
+//         line.classList.add("line");
+//         line.classList.add("colored");
+//         console.log(i);
+//     }
+// }
+
+// function createBoxes (){
+// for (let i = 0; i < numberOfBoxes; i++){
+//     createLines();
+// }
+// }
+// createBoxes();
+
+// }
+
+// createMediumGrid();
 
 
 
