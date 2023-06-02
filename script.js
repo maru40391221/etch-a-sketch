4a739dcf9925dc993b90d6d9a0109d8f4e85b79d
maru40391221 <polorll.1221@gmail.com>
2023-06-03 00:24:44 +0900

    done with the medium size
    Work on the small next time

--- script.js
+++ script.js
@@ -1,65 +1,60 @@
+
 const container = document.querySelector(".container");
-let column;
-
-// create columns
-const numberOfColumn = 16;
-Array.from({length: numberOfColumn}, () => {
-    let box = document.createElement("div")
-    column.appendChild(box);
-    container.appendChild(column);
-    column.classList.add("column");
-});
-
-// create rows
-
-const numberOfRow = 16;
-Array.from({length: numberOfrow}, () => {
-
-    container.appendChild(column);
-    column.classList.add("column");
-});
+const smallButton = document.querySelector("#smallButton");
+const mediumButton = document.querySelector("#mediumButton");
+const largeButton = document.querySelector("#largeButton");
+const resetButton = document.querySelector("#reset");
 
 
 
+// smallButton.onclick = small;
+mediumButton.onclick = medium;
+// largeButton.onclick = large;
+resetButton.onclick = () => {
+    location.reload();
+};
+
+//////******************************MEDIUM****************************************///////
+//when mediumButton is clicked the following function gets called
+
+function medium () {
+
+//change the number below to change the number of boxes and the number of columns that contains the boxes
+    let numberOfBoxes = 16;
+    let numberOfLines = 16;
 
 
+function createMediumGrid (){
 
-//  the codes below are the first attempt with grid-layout. I failed. Now I will try to make 16x16 grids with flexbox-layout. So they can shrink and whatnot. 
-// const container = document.querySelector(".container");
-// const smallButton = document.querySelector("#smallButton");
-// const mediumButton = document.querySelector("#mediumButton");
-// const largeButton = document.querySelector("#largeButton");
+//creating a line of boxes horizontally
+    function createLines (){
+    for (let i = 0; i < numberOfLines; i++){
+        const line = document.createElement("div");
+        container.appendChild(line);
+        line.classList.add("line");
+        line.classList.add("colored");
+        console.log(i);
+    }
+}
 
-// let numberOfBoxes = 16;
-// let numberOfLines = 16;
+function createBoxes (){
+for (let i = 0; i < numberOfBoxes; i++){
+    createLines();
+}
+}
+createBoxes();
 
-// // container.style.display = "grid";
-// // container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 0fr)`; 
-// // mediumButton.onclick = ;
+}
+
+createMediumGrid();
+
+mediumButton.disabled = true;
+smallButton.style.display = "none";
+largeButton.style.display = "none";
 
 
-// function createMediumGrid (){
-// //creating a line of boxes horizontally
-// function createLines (){
-//     for (let i = 0; i < numberOfLines; i++){
-//         const line = document.createElement("div");
-//         container.appendChild(line);
-//         line.classList.add("line");
-//         line.classList.add("colored");
-//         console.log(i);
-//     }
-// }
-
-// function createBoxes (){
-// for (let i = 0; i < numberOfBoxes; i++){
-//     createLines();
-// }
-// }
-// createBoxes();
-
-// }
-
-// createMediumGrid();
+}
+//////**********************************************************************///////
 
 
 
