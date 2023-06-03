cb38dae33af58a18474bf5d70f845fd18d6c6ae3
maru40391221 <polorll.1221@gmail.com>
2023-06-03 15:25:46 +0900

    fixed small, medium, and large grid
    Next time I am gonna add a feature to change the color of the brush

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ const largeButton = document.querySelector("#largeButton");
 const resetButton = document.querySelector("#reset");
 
 
-
-// smallButton.onclick = small;
+smallButton.onclick = small;
 mediumButton.onclick = medium;
-// largeButton.onclick = large;
+largeButton.onclick = large;
 resetButton.onclick = () => {
     location.reload();
 };
@@ -20,25 +19,24 @@ resetButton.onclick = () => {
 function medium () {
 
 //change the number below to change the number of boxes and the number of columns that contains the boxes
-    let numberOfBoxes = 16;
-    let numberOfLines = 16;
+    let numberOfBoxinColumn = 16;
+    let numberOfBoxinRow = 16;
 
 
 function createMediumGrid (){
 
 //creating a line of boxes horizontally
     function createLines (){
-    for (let i = 0; i < numberOfLines; i++){
+    for (let i = 0; i < numberOfBoxinRow; i++){
         const line = document.createElement("div");
         container.appendChild(line);
-        line.classList.add("line");
+        line.classList.add("mediumLine");
         line.classList.add("colored");
-        console.log(i);
     }
 }
 
 function createBoxes (){
-for (let i = 0; i < numberOfBoxes; i++){
+for (let i = 0; i < numberOfBoxinColumn; i++){
     createLines();
 }
 }
@@ -54,8 +52,95 @@ largeButton.style.display = "none";
 
 
 }
-//////**********************************************************************///////
 
 
+
+
+//////*********************************SMALL*************************************///////
+//when smallButton is clicked the following function gets called
+
+function small () {
+
+    //change the number below to change the number of boxes and the number of columns that contains the boxes
+        let numberOfBoxinColumn = 30;
+        let numberOfBoxinRow = 32;
+    
+    
+    function createMediumGrid (){
+    
+    //creating a line of boxes horizontally
+        function createLines (){
+        for (let i = 0; i < numberOfBoxinRow; i++){
+            const line = document.createElement("div");
+            container.appendChild(line);
+            line.classList.add("smallLine");
+            line.classList.add("colored");
+            console.log(i);
+        }
+    }
+    
+    function createBoxes (){
+    for (let i = 0; i < numberOfBoxinColumn; i++){
+        createLines();
+    }
+    }
+    createBoxes();
+    
+    }
+    
+    createMediumGrid();
+    
+    smallButton.disabled = true;
+    mediumButton.style.display = "none";
+    largeButton.style.display = "none";
+    container.style.display = "grid";  
+    container.style.gridTemplateColumns = "repeat(32, 0fr)";
+    
+    
+    }
+
+
+
+
+
+//////*********************************LARGE*************************************///////
+
+function large () {
+
+    //change the number below to change the number of boxes and the number of columns that contains the boxes
+        let numberOfBoxinColumn = 16;
+        let numberOfBoxinRow = 8;
+    
+    
+    function createMediumGrid (){
+    
+    //creating a line of boxes horizontally
+        function createLines (){
+        for (let i = 0; i < numberOfBoxinRow; i++){
+            const line = document.createElement("div");
+            container.appendChild(line);
+            line.classList.add("largeLine");
+            line.classList.add("colored");
+            console.log(i);
+        }
+    }
+    
+    function createBoxes (){
+    for (let i = 0; i < numberOfBoxinColumn; i++){
+        createLines();
+    }
+    }
+    createBoxes();
+    
+    }
+    
+    createMediumGrid();
+    
+    largeButton.disabled = true;
+    smallButton.style.display = "none";
+    mediumButton.style.display = "none";
+    
+    
+    }
 
 
